f0ee7d5a68a82705f8a3d5ea94060ba09b1b2b4e
Rene Duquesnoy <rduquesnoy@drwholdings.com>
2018-12-06 08:43:48 -0600

    post message to slack when a new blog post occurs

---
+++ blog.py
@@ -0,0 +1,35 @@
+from slackclient import SlackClient
+import requests
+import os
+import time
+import json
+import configparser
+import twitter
+
+def send_message(msg):
+    c = configparser.ConfigParser()
+    c.read("config.ini")
+    
+    #to slack
+    slack_token = c["Prod"]["SLACK_API_TOKEN"]
+    sc = SlackClient(slack_token)
+
+    sc.api_call(
+      "chat.postMessage",
+      channel="C0W7NPCTY", #pathfinder
+      #channel="CCEM7951Q", #testrene
+      text=msg
+    )
+    
+
+with open("blog.txt", "r") as blog_file:
+    old_date=blog_file.read()
+
+r = requests.get('https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fpaizo.com%2Fcommunity%2Fblog%26xml%3Datom&api_key=beiorasshpurtgmjcyl2glbiywgcfbiap5uvsst8', timeout=5)
+latest_blog = r.json()['items'][0]
+
+if latest_blog['pubDate'] != old_date:
+    f = open("blog.txt", "w") 
+    f.write(latest_blog['pubDate'])
+    f.close()
+    send_message("New blog: <{}|{}>".format(latest_blog['link'], latest_blog['title']))
